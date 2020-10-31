da31f5839a3a5679b5e8e0d37f30baf7c25df90a
HIROSHI84 <hiroshi4984@gmail.com>
2020-10-31 12:46:26 +0900

    ユーザー編集機能　漏れ分3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: 'users#edit'
+  
+  root 'items#index'
+  #resources :users
+
+  
+  # root to: 'items#index'
   # resources :items, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
