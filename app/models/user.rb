b84a1df7862c5395168a2fbb055d007932fb8407
HIROSHI84 <hiroshi4984@gmail.com>
2020-11-03 16:06:42 +0900

    ユーザー編集機能　漏れ分８

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,14 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
+  validates :password, format: { with: /\A[a-z0-9]+\z/i, message: 'は半角英数で入力してください。' }
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' }
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください。' }
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力して下さい。' }
   validates :birth_day, presence: true
 end
